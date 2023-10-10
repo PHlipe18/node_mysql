face1386d48f4524c28f537990d836c49cabe4b9
philipe <philippsilva018@gmail.com>
2023-10-10 20:47:28 -0300

    primeiro commit sla

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql2")
+
+const app = express()
+
+app.engine("Handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.get("/", (requisicao, resposta) => {
+    resposta.render("home")
+})
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql",
+    port: 3307
+}) 
+
+conn.connect((error) => {
+    if (error){
+        console.log(error)
+        return
+    }
+    console.log("conectado ao MySQL!")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000")
+    })
+})
