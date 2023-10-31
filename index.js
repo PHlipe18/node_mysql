eb3f1afc1a240811df375d044021d0d04e6b74ce
EA - Philipe Da Silva <philipe_silva@educadventista.org>
2023-10-31 20:25:38 -0300

    atualizando banco de dados

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const { request, response } = require("express")
 const express = require("express")
 const exphbs = require("express-handlebars")
 const mysql = require("mysql2")
 
 const app = express()
 
-app.engine("Handlebars", exphbs.engine())
+app.engine("handlebars",exphbs.engine())
 app.set("view engine", "handlebars")
 
+
 app.use(express.static("public"))
+
 
 app.use(express.urlencoded({
     extended: true
@@ -16,30 +17,65 @@ app.use(express.urlencoded({
 
 app.use(express.json())
 
-app.get("/register", (request, response) =>{
-    response.render("register")
+
+
+app.post("/cadastrar/save", (request, response) =>{
+    
+    const {title,pageqty } = request.body
+    
+    
+    const query = `
+        INSERT INTO books (title,pageqty)
+        VALUES('${title}', '${pageqty}')
+    `
+
+    conn.query(query, (error)=>{
+        if (error){
+            console.log(error)
+            return
+        }
+        response.redirect("/")
+    })
 })
 
-app.get("/", (req, res) => {
-    res.render("home")
+app.get("/cadastrar", (request,response) =>{
+    response.render("cadastrar")
 })
 
+
+
+
+app.get("/", (req,res)=>{
+    const sql = 'SELECT * FROM books'
+    
+    conn.query(sql, (error, data) => {
+        if (error){
+            return console.log(error)
+        }
+        const books = data
+        
+        res.render("home", { books })
+    })
+
+    
+}) 
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password:"root",
     database: "nodemysql",
-    port: 3307
-}) 
+    port:3307
+})
 
-conn.connect((error) => {
-    if (error){
+conn.connect((error)=>{
+    if(error){
         console.log(error)
         return
     }
-    console.log("conectado ao MySQL!")
 
-    app.listen(3000, () => {
+    console.log("Conectado ao MySQL!")
+
+    app.listen(3000,()=>{
         console.log("Servidor rodando na porta 3000")
     })
 })
